0bdc172773c56fe750a33016b1f3a078f412c4db
darkquilius <57682169+darkquilius@users.noreply.github.com>
2020-04-11 12:31:03 -0400

    Add files via upload
    Working Timer
    ToDo:
    - reset functionality
    - change timestamps to be shorter

---
+++ script.js
@@ -0,0 +1,56 @@
+var h1 = document.getElementsByTagName('h2')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0,
+    minutes = 0,
+    hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+calcTotalTime()
+
+/* Start button */
+start.addEventListener("click", function() {
+    timer();
+    var timestamp = new Date();
+    $("#timerBox").append(`<p>start timestamp: ${timestamp}</p>
+        <br>`)
+})
+
+
+/* Stop button */
+stop.addEventListener("click", function() {
+    clearTimeout(t);
+    var timestamp = new Date();
+    $("#timerBox").append(`<p>stop timestamp: ${timestamp}</p>
+    <br>`)
+})
+
+/* Clear button */
+clear.onclick = function() {
+    h1.textContent = "00:00:00";
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    clearTimeout(t)
+}
