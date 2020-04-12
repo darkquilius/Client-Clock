8251888d4051fb5fc6e71d487523a5edf1e45319
Connie Tran <connietrandev@gmail.com>
2020-04-12 17:14:27 -0400

    Create table.js for table functionality

--- script.js
+++ script.js
@@ -95,3 +95,16 @@ function calcTotalTime() {
 }
 
 
+<<<<<<< HEAD
+=======
+$(document).on("keypress", "input", function(e) {
+    if (e.which == 13) {
+        var inputVal = $(this).val();
+        // Adds to client dropdown
+        $("#clientDropdown").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
+        $("#currentClient").text(inputVal);
+        // Adds to client filter
+        $("#clientList").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
+    }
+});
+>>>>>>> Create table.js for table functionality
