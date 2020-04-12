baaec24f340622965c4919aa1e56345ca7dbc4ae
Connie Tran <connietrandev@gmail.com>
2020-04-12 17:21:52 -0400

    Client name appears in dropdown on click

--- script.js
+++ script.js
@@ -95,14 +95,3 @@ function calcTotalTime() {
 }
 
 
-$(document).on("keypress", "input", function(e) {
-    if (e.which == 13) {
-        var inputVal = $(this).val();
-        // Adds to client dropdown
-        $("#clientDropdown").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
-        $("#currentClient").text(inputVal);
-        // Adds to client filter
-        $("#clientList").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
-        $("input").val("");
-    }
-});
