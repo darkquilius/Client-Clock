d47e02af6f75adfe98fa977fec0e652ca4c22709
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-04-12 15:09:39 -0400

    proper changes for master

--- script.js
+++ script.js
@@ -46,24 +46,23 @@ stop.addEventListener("click", function() {
     var timestamp = moment().format("L HH:mm:ss");
     $("#timestampSlot").append(`<p>Stop Timestamp: ${timestamp}</p>
     <br>`)
-    $("#timestampSlot").append(`<p>Total Time: ${hours} hours ${minutes} minutes</p>
-    <br>`)
 })
 
 /* Clear button */
 clear.onclick = function() {
     calcTotalTime()
+    populateTable()
     h1.textContent = "00:00:00";
     seconds = 0;
     minutes = 0;
     hours = 0;
     clearTimeout(t);
-    // $("#timestampSlot").empty();
+    $("#timestampSlot").empty();
 }
 
 
 var clientName = "client 1"
-
+    // Easy storing of seconds
 function timeToSeconds() {
     minutes = (hours * 60) + minutes;
     hours = 0;
@@ -71,24 +70,35 @@ function timeToSeconds() {
     minutes = 0
 }
 
+// Returns seconds to legible time
 function secondsToTime() {
     var baseNum = localStorage.getItem("client 1");
     baseNum = parseInt(baseNum);
     hours = Math.floor(baseNum / 60 / 60);
     minutes = Math.floor((baseNum / 60) - (hours * 60));
-    console.log(hours);
-    console.log(minutes);
-
 }
 
+// Sets local storage and parses time for table. local storage to be used in recal of client time
 function calcTotalTime() {
-    // hours = 1;
-    // minutes = 10;
-    // seconds = 30;
     timeToSeconds()
     var clientTime = JSON.stringify(seconds)
     localStorage.setItem(clientName, clientTime)
     console.log(localStorage.getItem("client 1"));
     secondsToTime();
-
 }
+
+// Populates total time spent table
+function populateTable() {
+    var newRow = $("tbody").append(`<tr></tr>`)
+    newRow.append(`<td>${clientName}</td>`)
+    newRow.append(`<td>${hours} hours ${minutes} minutes</td>`)
+}
+
+$(document).on("keypress", "input", function(e) {
+    if (e.which == 13) {
+        var inputVal = $(this).val();
+        $("#clientDropdown").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
+        $("#currentClient").text(inputVal);
+        $("#clientList").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
+    }
+});
