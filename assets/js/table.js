c856c7ce83862b8ec758a28677df04a14aaeb9ef
Connie Tran <connietrandev@gmail.com>
2020-04-13 15:37:07 -0400

    Create entry object and push onto allEntries array to populate table with new time entry

--- assets/js/table.js
+++ assets/js/table.js
@@ -3,9 +3,20 @@
 function populateTable() {
     var newRow = $("tbody").append(`<tr></tr>`);
 
-    var currentClient = $("#currentClient").text();
+    // Client name column
+    var currentClient = allEntries[allEntries.length - 1].client;
     newRow.append(`<td>${currentClient}</td>`);
 
+    // Hours column
     newRow.append(`<td>${hours} hours ${minutes} minutes</td>`);
+ 
+    // Cost column
+    newRow.append(`<td>Cost</td>`);
+
+    // Time column
+    var startTime = moment(allEntries[allEntries.length - 1].startTime).format("h:mm:ss");
+    var stopTime = moment(allEntries[allEntries.length - 1].stopTime).format("h:mm:ss");
+
+    newRow.append(`<td>${startTime} - ${stopTime}</td>`);
     
 }
