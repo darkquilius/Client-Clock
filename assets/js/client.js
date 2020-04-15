cacd8538a5ff299b89f4cb40e12b2c68501dc2de
Karolyn Seredick <kseredick95@gmail.com>
2020-04-15 10:50:38 -0400

    Array time, input drop down fix

--- assets/js/client.js
+++ assets/js/client.js
@@ -21,7 +21,8 @@ function priorClient() {
 
         $("#clientDropdown").prepend(`<a class="dropdown-item" href="#">${prev}</a>`);
         $("#currentClient").text(prev);
-    } else {
+    }
+    else {
         clientSaved = JSON.parse(prev)
 
         console.log(clientSaved)
@@ -30,7 +31,6 @@ function priorClient() {
         clientSaved.forEach(client => {
             $("#clientDropdown").prepend(`<a class="dropdown-item" index=${index} data-id=${index} href="#">${client}</a>`);
             $("#currentClient").text(client);
-            $("input").val("");
 
             index++;
         });
@@ -39,7 +39,7 @@ function priorClient() {
 
 
 // When client is entered into input, add to dropdown
-$(document).on("keypress", "input", function(e) {
+$(document).on("keypress", "input", function (e) {
     if (e.which == 13) {
         var inputVal = $(this).val();
 
@@ -50,7 +50,7 @@ $(document).on("keypress", "input", function(e) {
 });
 
 // When item in dropdown is clicked, set dropdown button to event.innerText
-$(document).on('click', '.dropdown-menu a', function() {
+$(document).on('click', '.dropdown-menu a', function () {
     $("#currentClient").text(this.innerText);
 });
 
@@ -65,13 +65,14 @@ function dublicateCheck(inputVal) {
     clientSaved.forEach(client => {
 
         console.log(client)
-            //makes client names lowercase
+        //makes client names lowercase
         var lowClient = client.toLowerCase();
 
         if (lowClient == lowerInput) {
             console.log("They're the same")
             x += 1;
-        } else {
+        }
+        else {
             console.log("They're different")
         }
     });
@@ -79,7 +80,9 @@ function dublicateCheck(inputVal) {
     if (x == 0) {
         addName(inputVal);
         createObject(inputVal);
-    } else {}
+    }
+    else {
+    }
 }
 
 function addName(inputVal) {
@@ -87,10 +90,8 @@ function addName(inputVal) {
     clientSaved.push(inputVal);
 
     //Adds to client dropdown
-    $("#clientDropdown").prepend(`<a class="dropdown-item" index=${clientSaved.length - 1} data-id=${clientSaved.length - 1} href="#"></a>`);
-    $("#clientList").prepend(`<a class="dropdown-item" href="#">${inputVal}</a>`);
+    $("#clientDropdown").prepend(`<a class="dropdown-item active" index=${clientSaved.length - 1} id= "dropdown-button" data-id=${clientSaved.length - 1} href="#">${inputVal}</a>`);
     $("#currentClient").text(inputVal);
-    $("input").val("");
 
     //Saves to Storage
     localStorage.setItem("clientName", JSON.stringify(clientSaved));
@@ -101,10 +102,7 @@ function createObject(inputVal) {
 
     obj.client.push({
         ID: inputVal,
-        startTime: 00,
-        stopTime: 00,
-        totalTime: 00,
-        cost: 00
+        startTime: [], stopTime:[], totalTime:[], cost: 00
     })
 
     localStorage.setItem("objectClient", JSON.stringify(obj))
