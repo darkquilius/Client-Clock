e5fbbaf728cc1342afef4230e71a29a3255008ae
Karolyn Seredick <kseredick95@gmail.com>
2020-04-17 10:27:08 -0400

    rebase

--- assets/js/client.js
+++ assets/js/client.js
@@ -21,7 +21,8 @@ function priorClient() {
 
         $("#clientDropdown").prepend(`<a class="dropdown-item" href="#">${prev}</a>`);
         $("#currentClient").text(prev);
-    } else {
+    }
+    else {
         clientSaved = JSON.parse(prev)
 
         console.log(clientSaved)
@@ -33,44 +34,22 @@ function priorClient() {
 
             index++;
         });
-        // boogaBooga()
     }
 }
 
-// Get all buttons with class="btn" inside the container
-var btns = document.getElementsByClassName("dropdown-item")
-
-// function updateDropdownLength() {
-//     btnsLength = document.getElementsByClassName("dropdown-item").length
-// }
-
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-
-        // If there's no active class
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace(" active", "");
-        }
-
-        // Add the active class to the current/clicked button
-        this.className += " active";
-    });
-}
 
 // When client is entered into input, add to dropdown
-$(document).on("keypress", "#client-input", function(e) {
+$(document).on("keypress", "input", function (e) {
     if (e.which == 13) {
         var inputVal = $(this).val();
 
         //Checks for dublicates
         dublicateCheck(inputVal);
-        boogaBooga();
     }
 });
 
 // When item in dropdown is clicked, set dropdown button to event.innerText
-$(document).on('click', '.dropdown-menu a', function() {
+$(document).on('click', '.dropdown-menu a', function () {
     $("#currentClient").text(this.innerText);
 });
 
@@ -87,13 +66,14 @@ function dublicateCheck(inputVal) {
     clientSaved.forEach(client => {
 
         console.log(client)
-            //makes client names lowercase
+        //makes client names lowercase
         var lowClient = client.toLowerCase();
 
         if (lowClient == lowerInput) {
             console.log("They're the same")
             x += 1;
-        } else {
+        }
+        else {
             console.log("They're different")
         }
     });
@@ -101,7 +81,9 @@ function dublicateCheck(inputVal) {
     if (x == 0) {
         addName(inputVal);
         createObject(inputVal);
-    } else {}
+    }
+    else {
+    }
 }
 
 function addName(inputVal) {
@@ -109,46 +91,19 @@ function addName(inputVal) {
     clientSaved.push(inputVal);
 
     //Adds to client dropdown
-    $("#clientDropdown").prepend(`<a class="dropdown-item" index=${clientSaved.length - 1} data-id=${clientSaved.length - 1} href="#">${inputVal}</a>`);
+    $("#clientDropdown").prepend(`<a class="dropdown-item active" index=${clientSaved.length - 1} id= "dropdown-button" data-id=${clientSaved.length - 1} href="#">${inputVal}</a>`);
     $("#currentClient").text(inputVal);
-    $("input").val("")
 
     //Saves to Storage
     localStorage.setItem("clientName", JSON.stringify(clientSaved));
-    boogaBooga();
-    populateObjectArray()
-}
-
-function boogaBooga() {
-    var a = document.getElementById("clientDropdown").children
-    var b = a[0].setAttribute("class", "dropdown-item active")
-    for (let i = 1; i < clientSaved.length; i++) {
-        const element = a[i];
-        element.setAttribute("class", "dropdown-item")
-    }
 }
 
 function createObject(inputVal) {
 
-    obj.push({
-        ID: inputVal,
-        startTime: [],
-        stopTime: [],
-        totalTime: 00,
-        cost: 0
+    obj.client.push({
+        ID : inputVal,
+        startTime : [], stopTime : [], totalTim : [], cost : 00
     })
 
     localStorage.setItem("objectClient", JSON.stringify(obj))
 }
-
-function populateObjectArray() {
-    var prev = JSON.parse(localStorage.getItem("objectClient"));
-
-    if (prev == null) {
-        obj = []
-    } else {
-        obj = prev
-    }
-}
-
-document.getElementById("clientDropdown").children[0].setAttribute("class", "dropdown-item active")
