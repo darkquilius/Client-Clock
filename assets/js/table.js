eafba4a1883e3b7dd7df67194df1f08a036a172c
Logan Hemphill <logan.hemphill86@gmail.com>
2020-04-17 10:10:06 -0400

    Cleaned up Table.js file

--- assets/js/table.js
+++ assets/js/table.js
@@ -9,29 +9,5 @@ function populateTable() {
     console.log(startStamp)
     console.log(stopStamp)
     3
-    // var newRow = $("tbody").append(`<tr></tr>`);
-
-    // var currentClient = $(".active").text();
-    // newRow.append(`<td>${currentClient}</td>`);
-
-    // newRow.append(`<td>${hours} hours ${minutes} minutes</td>`);
-
-    // // Cost column
-    // newRow.append(`<td>${a[activeIndex].totalTime}</td>`);
-
-    // // Time column
-    // newRow.append(`<td>${startStamp} - ${stopStamp}</td>`);
 
 }
-
-
-// // Billing rate keypress input
-// $(document).on("keypress", "#billingrate", function(e) {
-//     if (e.which == 13) {
-//         rate = $(this).val();
-//         $(this).val(""); 
-
-//         // Billing rate log
-//         console.log("The billing rate is: $" + rate + "/hr");
-//     }
-// });
